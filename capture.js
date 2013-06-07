1d2c1b114c4617011fdc1e87d692be3e0551497a
Frankmiky <franksimus@yahoo.fr>
2013-06-07 16:03:01 +0300

    Update capture.js

--- capture.js
+++ capture.js
@@ -6,72 +6,53 @@
     document.addEventListener("deviceready",onDeviceReady,false);
  
     // PhoneGap is ready to be used!
-    //
-    function onDeviceReady() {
+    function onDeviceReady() 
+    { 
         pictureSource=navigator.camera.PictureSourceType;
         destinationType=navigator.camera.DestinationType;
     }
  
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoDataSuccess(imageData) {
+    // Called when a photo is successfully retrieved DATA_URL
+    function onPhotoDataSuccess(imageData)
+    {
       // Get image handle
-      //
       var smallImage = document.getElementById('smallImage');
- 
-      // Unhide image elements
-      //
+     // Unhide image elements
       smallImage.style.display = 'block';
- 
       // Show the captured photo
       // The inline CSS rules are used to resize the image
-      //
       smallImage.src = "data:image/jpeg;base64," + imageData;
     }
     
-  // Called when a photo is successfully retrieved
-    //
-    function onPhotoFileSuccess(imageData) {
+    // Called when a photo is successfully retrieved DATA_URI
+    function onPhotoFileSuccess(imageData) 
+    {
       // Get image handle
       console.log(JSON.stringify(imageData));
-      
-       // Get image handle
-      //
+      // Get image handle
       var smallImage = document.getElementById('smallImage');
- 
       // Unhide image elements
-      //
       smallImage.style.display = 'block';
- 
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
+      // Show the captured photo ,The inline CSS rules are used to resize the image
       smallImage.src = imageData;
     }
  
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoURISuccess(imageURI) {
+    // Called when a photo is successfully retrieved (DATA_URI) from Library oder Album not from Camera
+    function onPhotoURISuccess(imageURI) 
+    {
       // Uncomment to view the image file URI 
       //console.log(imageURI);
- 
       // Get image handle
-      //
       var largeImage = document.getElementById('largeImage');
- 
       // Unhide image elements
-      //
       largeImage.style.display = 'block';
- 
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
+      // Show the captured photo      // The inline CSS rules are used to resize the image
       largeImage.src = imageURI;
     }
  
     // A button will call this function
-    //
-    function capturePhotoWithData() {
+    function capturePhotoWithData()
+    {
       // Take picture using device camera and retrieve image as base64-encoded string
       navigator.camera.getPicture(onPhotoDataSuccess, onFail,
         { quality: 50 ,
@@ -79,7 +60,8 @@
         });
     }
  
-    function capturePhotoWithFile() {
+    function capturePhotoWithFile()
+    {
         navigator.camera.getPicture(onPhotoFileSuccess, onFail,
         { quality: 50 ,
           destinationType: destinationType.FILE_URI
@@ -87,8 +69,8 @@
     }
     
     // A button will call this function
-    //
-    function getPhoto(source) {
+    function getPhoto(source)
+    {
       // Retrieve image file location from specified source
       navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
         destinationType: destinationType.FILE_URI,
@@ -96,7 +78,6 @@
     }
  
     // Called if something bad happens.
-    // 
     function onFail(message) {
       alert('Failed because: ' + message);
     }
